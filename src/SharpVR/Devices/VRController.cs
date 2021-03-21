2f6063dab01ee1b9d80069e51d6a025b9dc93783
TruDan <trudan97@gmail.com>
2021-03-21 12:39:48 +0100

    refactor

--- src/SharpVR/Devices/VRController.cs
+++ src/SharpVR/Devices/VRController.cs
@@ -20,11 +20,7 @@ namespace SharpVR
         private VRControllerState _currentState;
 
         public Quaternion ControllerOffset { get; set; }
-        public bool ButtonPressed(EVRButtonId button)
-        {
-            return (_lastState.ulButtonPressed & (1ul << (int) button)) == 0 &&
-                   (_currentState.ulButtonPressed & (1ul << (int) button)) != 0;
-        }
+
 
         public override void Update()
         {
@@ -73,29 +69,6 @@ namespace SharpVR
             }
         }
 
-        public bool GetPress(ulong buttonMask) => (_currentState.ulButtonPressed & buttonMask) != 0;
-
-        public bool GetPressDown(ulong buttonMask) => (_currentState.ulButtonPressed & buttonMask) != 0 &&
-                                                      (_lastState.ulButtonPressed & buttonMask) == 0;
-
-        public bool GetPressUp(ulong buttonMask) => (_currentState.ulButtonPressed & buttonMask) == 0 &&
-                                                    (_lastState.ulButtonPressed & buttonMask) != 0;
-
-        public bool GetPress(EVRButtonId     buttonId) => GetPress(1ul << (int) buttonId);
-        public bool GetPressDown(EVRButtonId buttonId) => GetPressDown(1ul << (int) buttonId);
-        public bool GetPressUp(EVRButtonId   buttonId) => GetPressUp(1ul << (int) buttonId);
-
-        public bool GetTouch(ulong buttonMask) => (_currentState.ulButtonTouched & buttonMask) != 0;
-
-        public bool GetTouchDown(ulong buttonMask) => (_currentState.ulButtonTouched & buttonMask) != 0 &&
-                                                      (_lastState.ulButtonTouched & buttonMask) == 0;
-
-        public bool GetTouchUp(ulong buttonMask) => (_currentState.ulButtonTouched & buttonMask) == 0 &&
-                                                    (_lastState.ulButtonTouched & buttonMask) != 0;
-
-        public bool GetTouch(EVRButtonId     buttonId) => GetTouch(1ul << (int) buttonId);
-        public bool GetTouchDown(EVRButtonId buttonId) => GetTouchDown(1ul << (int) buttonId);
-        public bool GetTouchUp(EVRButtonId   buttonId) => GetTouchUp(1ul << (int) buttonId);
 
         public Ray GetPointer()
         {
@@ -114,7 +87,8 @@ namespace SharpVR
         public Ray GetNextPointer()
         {
             var r = NextLocalRotation; //Quaternion.Multiply(LocalRotation, ControllerOffset);
-            var p = GetNextPose();//Matrix.Identity * Matrix.CreateFromQuaternion(r) * Matrix.CreateTranslation(LocalPosition);
+            var
+                p = GetNextPose(); //Matrix.Identity * Matrix.CreateFromQuaternion(r) * Matrix.CreateTranslation(LocalPosition);
             var nearPoint = Vector3.Transform(Vector3.Zero, p);
             var farPoint  = Vector3.Transform(Vector3.Forward, p);
             var direction = farPoint - nearPoint;
@@ -125,16 +99,37 @@ namespace SharpVR
             return new Ray(NextLocalPosition, direction);
         }
 
-        public void Vibrate()
-        {
-            Vibrate(TimeSpan.FromSeconds(0.255f));
-        }
-        
-        public void Vibrate(TimeSpan duration)
-        {
-            Context.System.TriggerHapticPulse((uint)Index, 0, (ushort)(duration.TotalMilliseconds * 1000));
-        }
+        public bool GetPress(ulong buttonMask) => (_currentState.ulButtonPressed & buttonMask) != 0;
 
+        public bool GetPressDown(ulong buttonMask) => (_currentState.ulButtonPressed & buttonMask) != 0 &&
+                                                      (_lastState.ulButtonPressed & buttonMask) == 0;
+
+        public bool GetPressUp(ulong buttonMask) => (_currentState.ulButtonPressed & buttonMask) == 0 &&
+                                                    (_lastState.ulButtonPressed & buttonMask) != 0;
+
+        public bool GetPress(EVRButtonId     buttonId) => GetPress(1ul << (int) buttonId);
+        public bool GetPressDown(EVRButtonId buttonId) => GetPressDown(1ul << (int) buttonId);
+        public bool GetPressUp(EVRButtonId   buttonId) => GetPressUp(1ul << (int) buttonId);
+        public bool ButtonPressed(EVRButtonId button) =>
+            (_lastState.ulButtonPressed & (1ul << (int) button)) == 0 &&
+            (_currentState.ulButtonPressed & (1ul << (int) button)) != 0;
+
+        public bool GetTouch(ulong buttonMask) => (_currentState.ulButtonTouched & buttonMask) != 0;
+
+        public bool GetTouchDown(ulong buttonMask) => (_currentState.ulButtonTouched & buttonMask) != 0 &&
+                                                      (_lastState.ulButtonTouched & buttonMask) == 0;
+
+        public bool GetTouchUp(ulong buttonMask) => (_currentState.ulButtonTouched & buttonMask) == 0 &&
+                                                    (_lastState.ulButtonTouched & buttonMask) != 0;
+
+        public bool GetTouch(EVRButtonId     buttonId) => GetTouch(1ul << (int) buttonId);
+        public bool GetTouchDown(EVRButtonId buttonId) => GetTouchDown(1ul << (int) buttonId);
+        public bool GetTouchUp(EVRButtonId   buttonId) => GetTouchUp(1ul << (int) buttonId);
+
+        public void Vibrate() => Vibrate(TimeSpan.FromSeconds(0.255f));
+
+        public void Vibrate(TimeSpan duration) =>
+            Context.System.TriggerHapticPulse((uint) Index, 0, (ushort) (duration.TotalMilliseconds * 1000));
 
         internal VRController(VrContext context, int index) : base(context, index)
         {
@@ -151,7 +146,8 @@ namespace SharpVR
                     Hand = Hand.None;
                     break;
             }
-            ControllerOffset = Quaternion.CreateFromYawPitchRoll(0f, -((float)Math.PI/2f), 0f);
+
+            ControllerOffset = Quaternion.CreateFromYawPitchRoll(0f, -((float) Math.PI / 2f), 0f);
         }
     }
 }
